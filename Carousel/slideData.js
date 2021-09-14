25b2a9cbc50d67427c1078c21fc22ac70310dd78
pratikartik <kartikshikhare26@gmail.com>
2021-09-14 13:07:39 +0530

    carousel

---
+++ Carousel/slideData.js
@@ -0,0 +1,46 @@
+export const slideData = [
+	{
+		image: "https://i.ibb.co/TMMsgdW/1.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/27bCjb4/2.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/z7xN84S/3.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/XjDdhJd/4.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/7yxr5Q7/5.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/XLVDwNG/6.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/Yk4Wy0K/7.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/6Y3SFWp/8.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/Kbb28CY/9.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/XzM1FM2/10.png",
+		title: "Covid 19",
+	},
+	{
+		image: "https://i.ibb.co/87RyZ8q/11.png",
+		title: "Covid 19",
+	},
+];
